5c0a4d4987f3402bf7bd3b3fb41b0f28ce9f1952
Jodiel Fabricio <jodiel_santos@hotmail.com>
2020-01-19 02:40:52 -0300

    Create version mobile

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello OmniStack by Jodiel!!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: '#fff',
+  },
+  
+});
